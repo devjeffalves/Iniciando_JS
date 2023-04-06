8c274c17d36f3a53c5e81a2178facd0c21632440
Dev Jeff Alves <jeff-alves@outlook.com>
2023-04-05 21:59:43 -0300

    Iniciando com JS

---
+++ script.js
@@ -0,0 +1,38 @@
+// meu primeiro comentário
+
+/*
+criando minha primeira pagina 
+
+
+*/
+
+console.log('Hello World');
+
+//prompt("Qual a sua idade");
+
+
+
+// conhecendo e trabalhando com variáveis
+
+const diaDeNascimento = 18;
+let texto = "Meu dia de nascimento eh: ";
+
+console.log(texto);
+console.log(diaDeNascimento);
+// concatenando variáveis
+console.log (texto , diaDeNascimento);
+
+
+// exemplos 
+
+const primeiroNome = "Jeferson";
+const meuSobrenome = "Alves";
+let minhaIdade = 37 ;
+let souEstudante = true;
+
+//Imprimir informações
+
+console.log('Meu nome é' , primeiroNome, meuSobrenome,'. Tenho' ,minhaIdade,
+'estou estudando:', souEstudante );
+
+
